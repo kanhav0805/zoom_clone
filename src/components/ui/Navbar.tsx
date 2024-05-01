37996481350925435a33ee5ad6849821f2ec3f83
Kanhav Sukhwal <kanhavsukhwal@gmail.com>
2024-05-01 18:47:50 +0530

    zoom clone

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
+const Navbar = () => {
+  return (
+    <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Zoom Lite Logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Zoom Lite
+        </p>
+      </Link>
+      <div className="flex flex-between gap-5">
+        {/* clerks user management */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
